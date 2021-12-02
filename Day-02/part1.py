053cfa9b9134985689ca991f7737cfcb8b904fca
Archan Patkar <pat@archanpatkar.com>
2021-12-02 10:44:25 +0530

    done

--- Day-02/part1.py
+++ Day-02/part1.py
@@ -3,8 +3,23 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    temp = d.split(" ")
+    return (temp[0],int(temp[1]))
 
 data = fnl(parse)
 
-print(data)
+# print(data)
+
+hor = 0
+dep = 0
+for ins in data:
+    if(ins[0] == "up"):
+        dep -= ins[1]
+    if(ins[0] == "down"):
+        dep += ins[1]
+    if(ins[0] == "forward"):
+        hor += ins[1]
+    # if(ins[0] == "backward"):
+    #     dep += ins[1]
+
+print(hor*dep)
--- Day-02/part2.py
+++ Day-02/part2.py
@@ -3,8 +3,27 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    if(len(d)> 0):
+        temp = d.split(" ")
+        return (temp[0],int(temp[1]))
+    return ("",0)
 
 data = fnl(parse)
 
-print(data)
+# print(data)
+
+hor = 0
+dep = 0
+aim = 0
+for ins in data:
+    if(ins[0] == "up"):
+        aim -= ins[1]
+    if(ins[0] == "down"):
+        aim += ins[1]
+    if(ins[0] == "forward"):
+        hor += ins[1]
+        dep += (aim * ins[1])
+    # if(ins[0] == "backward"):
+    #     dep += ins[1]
+
+print(hor*dep)
