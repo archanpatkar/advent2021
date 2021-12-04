edec269c1376ebe8df9989280b146ad056f483ba
Archan Patkar <pat@archanpatkar.com>
2021-12-04 10:17:30 +0530

    init

---
+++ Day-04/part1.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("..")
+from common import *
+
+def parse(d):
+    return d
+
+data = fnl(parse)
+
+print(data)
---
+++ Day-04/part2.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("..")
+from common import *
+
+def parse(d):
+    return d
+
+data = fnl(parse)
+
+# print(data)
+
