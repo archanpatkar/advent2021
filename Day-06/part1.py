4e0534eb10ed0dfd9b5ab0dda06e16f04d6704b7
Archan Patkar <pat@archanpatkar.com>
2021-12-06 11:29:18 +0530

    update

--- Day-06/part1.py
+++ Day-06/part1.py
@@ -3,8 +3,19 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    return [int(n) for n in d.strip().split(",")]
 
-data = fnl(parse)
+data = fnl(parse)[0]
+school = data
+print(school)
 
-# print(data)
+days = 80
+
+for day in range(days):
+    for fish in range(len(school)):
+        if(school[fish] == 0): 
+            school[fish] = 6
+            school.append(8)
+        else: school[fish] -= 1
+    
+print(len(school))
--- Day-06/part2.py
+++ Day-06/part2.py
@@ -3,8 +3,27 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    return [int(n) for n in d.strip().split(",")]
 
-data = fnl(parse)
+data = fnl(parse)[0]
+school = data
+print(data)
 
-# print(data)
+days = 256
+rate = 7
+track = [0 for i in range(rate+2)]
+
+for fish in range(len(school)):
+    track[school[fish]] += 1
+    
+pprint(track)
+
+for day in range(days):
+    day0 = track[0]
+    track = track[1:]
+    track.append(0)
+    if(day0 >= 1):
+        track[8] += day0
+        track[6] += day0
+
+print(sum(track))
