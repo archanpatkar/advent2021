6d44648e07ea41ea5f7764dc381cb677c9ee5c18
Archan Patkar <pat@archanpatkar.com>
2021-12-07 10:58:12 +0530

    done

--- Day-07/part1.py
+++ Day-07/part1.py
@@ -3,8 +3,18 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    return [int(n) for n in d.strip().split(",")]
 
-data = fnl(parse)
+data = aoci(parse)
 
-# print(data)
+print(data)
+
+all = []
+for pos in range(len(data)):
+    sum = 0
+    for crab in data:
+       sum += abs(pos-crab)
+    all.append(sum)
+    
+pprint(all)
+print(min(all))
--- Day-07/part2.py
+++ Day-07/part2.py
@@ -3,8 +3,21 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    return [int(n) for n in d.strip().split(",")]
 
-data = fnl(parse)
+data = aoci(parse)
 
-# print(data)
+print(data)
+
+def calc(n):
+    return (n*(n+1))//2
+
+all = []
+for pos in range(len(data)):
+    sum = 0
+    for crab in data:
+       sum += calc(abs(pos-crab))
+    all.append(sum)
+    
+pprint(all)
+print(min(filter(lambda x: x != 0,all)))
