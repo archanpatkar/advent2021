89c619e2c0810b8be97abe8294e7ce62f7fabeb6
Archan Patkar <pat@archanpatkar.com>
2021-12-10 12:35:13 +0530

    done

--- Day-10/part1.py
+++ Day-10/part1.py
@@ -2,9 +2,39 @@ import sys
 sys.path.append("..")
 from common import *
 
-def parse(d):
-    pass
+def iscorrupted(d):
+    return d
 
-data = fnl(parse)
+data = fnl(iscorrupted)
 
-# print(data)
+start = ["(","<","{","["]
+end = {
+    "(":")",
+    "<":">",
+    "{":"}",
+    "[":"]",
+}
+
+def iscorrupted(s):
+    if s[0] in start:
+        curr = s[0]
+        s = s[1:]
+        out = False
+        while len(s) > 0 and s[0] in start:
+            out,s = iscorrupted(s)
+            if out and len(s) > 1:
+                return (True,s)
+        if len(s) == 0: return (False,"")
+        if s[0] != end[curr] and len(s) > 1:
+            return (True,s)
+        return (False,s[1:])
+    return (False,s)
+
+points = {
+    ")":3,
+    "]":57,
+    "}":1197,
+    ">":25137,
+}
+
+print(sum([points[e[1][0]] for e in [iscorrupted(l) for l in data] if e[0]]))
--- Day-10/part2.py
+++ Day-10/part2.py
@@ -3,8 +3,65 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    return d
 
 data = fnl(parse)
 
-# print(data)
+start = ["(","<","{","["]
+end = {
+    "(":")",
+    "<":">",
+    "{":"}",
+    "[":"]",
+}
+
+def iscorrupted(s):
+    if s[0] in start:
+        curr = s[0]
+        s = s[1:]
+        out = False
+        while len(s) > 0 and s[0] in start:
+            out,s = iscorrupted(s)
+            if out and len(s) > 1:
+                return (True,s)
+        if len(s) == 0: return (False,"")
+        if s[0] != end[curr] and len(s) > 1:
+            return (True,s)
+        return (False,s[1:])
+    return (False,s)
+
+def _ac(s,completed=[]):
+    if s[0] in start:
+        curr = s[0]
+        s = s[1:]
+        while len(s) > 0 and s[0] in start:
+            s = _ac(s,completed)
+        if len(s) == 0: 
+            completed.append(end[curr])
+            return ""
+        elif s[0] == end[curr]: return s[1:]
+    return s
+
+def autocomplete(s,completed=[]):
+    while len(s) > 0:
+        s = _ac(s,completed)
+    return completed
+
+points = {
+    ")":3,
+    "]":57,
+    "}":1197,
+    ">":25137,
+}
+
+points2 = {
+    ")":1,
+    "]":2,
+    "}":3,
+    ">":4,
+}
+
+incomp = [data[i] for i in [ind for ind,o in enumerate([iscorrupted(l) for l in data]) if not o[0]]]
+scores = [reduce(lambda acc,v: acc*5 + points2[v],autocomplete(line,[]),0) for line in incomp]
+scores.sort()
+print(scores[len(scores)//2])
