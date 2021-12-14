892383fd56f3084dac632f3b64b0ca5f85741e14
Archan Patkar <pat@archanpatkar.com>
2021-12-14 12:15:34 +0530

    done

--- Day-14/part1.py
+++ Day-14/part1.py
@@ -3,8 +3,31 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    d = d.split("\n")
+    goal = d[0]
+    gens = {}
+    for i in d[2:]:
+        t = i.split("->")
+        gens[t[0].strip()] = t[1].strip()
+    return (goal,gens)
 
-data = fnl(parse)
+goal,ins = aoci(parse)
+pprint(goal)
+pprint(ins)
 
-# print(data)
+start = goal
+steps = 40
+for i in range(steps):
+    print("step:",i)
+    next = ""
+    for i in range(len(start)-1):
+        next += start[i]
+        if start[i] + start[i+1] in ins:    
+            next += ins[start[i] + start[i+1]]
+    next += start[-1]
+    start = next
+
+nch = freq(start)
+pprint(nch)
+all = [nch[k] for k in nch]
+print(max(all)-min(all))
--- Day-14/part2.py
+++ Day-14/part2.py
@@ -3,8 +3,40 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    d = d.split("\n")
+    goal = d[0]
+    gens = {}
+    for i in d[2:]:
+        t = i.split("->")
+        gens[t[0].strip()] = t[1].strip()
+    return (goal,gens)
 
-data = fnl(parse)
+goal,ins = aoci(parse)
+# pprint(goal)
+# pprint(ins)
 
-# print(data)
+def genpairs(str):
+    pairs = defaultdict(int)
+    ch = defaultdict(int)
+    for i in range(len(str)-1):
+        ch[str[i]] += 1
+        pairs[str[i] + str[i+1]] += 1
+    ch[str[i+1]] += 1
+    return pairs,ch
+
+pairs,ch = genpairs(goal)
+steps = 40
+for i in range(steps):
+    print("step:",i)
+    next = defaultdict(int)
+    for p in [*pairs.keys()]:
+        mid = ins[p]
+        ch[mid] += pairs[p]
+        next[p[0] + mid] += pairs[p]
+        next[mid + p[1]] += pairs[p]
+    pairs = next
+
+pprint(pairs)
+pprint(ch)
+all = [ch[k] for k in ch]
+print(max(all)-min(all))
