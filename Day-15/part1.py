4269ed43366abdd354371b83d7cac51d13c0ee4d
Archan Patkar <pat@archanpatkar.com>
2021-12-15 20:24:34 +0530

    done

--- Day-15/part1.py
+++ Day-15/part1.py
@@ -3,8 +3,50 @@ sys.path.append("..")
 from common import *
 
 def parse(d):
-    pass
+    return [int(n) for n in d]
 
 data = fnl(parse)
 
-# pprint(data)
+pprint(data)
+
+adj = lambda x,y: [(x,y+1),(x,y-1),(x-1,y),(x+1,y)]
+graph = defaultdict(lambda: [])
+risk_map = defaultdict(int)
+for y in range(len(data)):
+    for x in range(len(data[0])):
+        risk_map[(x,y)] = data[y][x]
+        graph[(x,y)] = adj(x,y)
+
+for n in graph: graph[n] = [n for n in graph[n] if n in risk_map]
+
+# pprint(risk_map)
+# pprint(graph)
+
+def manhdis(p1,p2):
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
+start = (0,0)
+end = (len(data[0])-1,len(data)-1)
+
+distance = defaultdict(lambda: -1)
+distance[start] = 0
+# path = []
+queue = [(0,start)]
+
+i = 0
+while len(queue) > 0:
+    cost,curr = queue.pop(0)
+    print("i:",i)
+    for next in graph[curr]:
+        ncost = cost + risk_map[next]
+        if not next in distance or distance[next] > ncost:
+            distance[next] = ncost
+            # path.append(next)
+            queue.append((ncost,next))
+    if(end in distance): break
+    queue.sort(key=lambda t: t[0])
+    i += 1
+
+print(distance[end])
+
+
--- Day-15/part2.py
+++ Day-15/part2.py
@@ -1,10 +1,54 @@
 import sys
+from threading import currentThread
 sys.path.append("..")
 from common import *
+from queue import PriorityQueue
 
 def parse(d):
-    pass
+    return [int(n) for n in d]
 
 data = fnl(parse)
 
-# pprint(data)
+final = []
+for i in range(5):
+    n = []
+    for r in data:
+        nr = []
+        for j in range(5):
+            nr.extend(map(lambda n: (n+i+j) if (n+i+j) <= 9 else ((n+i+j)%9),r))
+        n.append(nr)
+    final.extend(n)
+
+data = final
+
+adj = lambda x,y: [(x,y+1),(x,y-1),(x-1,y),(x+1,y)]
+graph = defaultdict(lambda: [])
+risk_map = defaultdict(int)
+for y in range(len(data)):
+    for x in range(len(data[0])):
+        risk_map[(x,y)] = data[y][x]
+        graph[(x,y)] = adj(x,y)
+
+for n in graph: graph[n] = [n for n in graph[n] if n in risk_map]
+
+def manhdis(p1,p2):
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
+start = (0,0)
+end = (len(data[0])-1,len(data)-1)
+
+queue = PriorityQueue()
+queue.put((0,start))
+distance = defaultdict(lambda: -1)
+distance[start] = 0
+
+while not queue.empty():
+    priority,current = queue.get()
+    if current[0] == end: break
+    for next in graph[current]:
+        cost = distance[current] + risk_map[next]
+        if (not next in distance) or cost < distance[next]:
+            distance[next] = cost
+            priority = cost + manhdis(next,end)
+            queue.put((priority,next))
+print(distance[end])
