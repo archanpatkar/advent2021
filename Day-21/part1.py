c021c9fdd97cf272826e78b4f9ed988837b37984
Archan Patkar <pat@archanpatkar.com>
2021-12-23 23:43:13 +0530

    done

--- Day-21/part1.py
+++ Day-21/part1.py
@@ -2,9 +2,34 @@ import sys
 sys.path.append("..")
 from common import *
 
-def parse(d):
-    pass
+dice = 1
+count = 0
+def throw_dice():
+    global dice
+    global count
+    rolls = []
+    for i in range(3):
+        rolls.append(dice)
+        dice += 1
+        if dice == 101: dice = 1
+        count += 1
+    return rolls
 
-data = fnl(parse)
+players = [6,1]
+scores = [0,0]
 
-# pprint(data)
+i = 0
+done = False
+while not done:
+    for p in range(len(players)):
+        n = sum(throw_dice())
+        print("pos:",players[p])
+        players[p] = (players[p] + n) % 10
+        if players[p] == 0: players[p] = 10
+        scores[p] += players[p]
+        if scores[p] >= 1000:
+            done = True
+            break
+    print(scores)
+
+print(min(scores)*count)
--- Day-21/part2.py
+++ Day-21/part2.py
@@ -1,10 +1,34 @@
 import sys
 sys.path.append("..")
 from common import *
+from functools import lru_cache
 
-def parse(d):
-    pass
+#        - 1  
+#       | 
+# dice -|- 2
+#       |
+#        - 3
 
-data = fnl(parse)
+players = (6,1)
+scores = [0,0]
 
-# pprint(data)
+rolls = defaultdict(int)
+
+for i in range(1,4):
+    for j in range(1,4): 
+        for k in range(1,4): rolls[sum((i,j,k))] += 1
+
+addt = lambda t1,t2: (t1[0]+t2[0],t1[1]+t2[1])
+
+@lru_cache(maxsize=None)
+def play(pos1,score1,pos2,score2):
+    out = []
+    for roll,count in rolls.items():
+        np = (pos1 + roll) % 10
+        if np == 0: np = 10
+        ns = score1 + np
+        if ns >= 21: out.append((count,0))
+        else: out.append(tuple(map(lambda v: v*count,play(pos2,score2,np,ns)))[::-1])
+    return reduce(addt,out)
+
+print(max(play(players[0],0,players[1],0)))
